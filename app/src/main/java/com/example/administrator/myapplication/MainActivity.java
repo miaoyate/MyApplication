8069ea1aa7fa4eccc42840a28c99455f9d217da8
yate <137581037@qq.com>
2016-10-23 17:34:03 +0800

    更新了一次代码，我的第二次更新

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        //what gost？
         button = (Button) findViewById(R.id.btn);
 
     }
